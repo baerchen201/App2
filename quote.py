540b0c98cf6179cbbd79fffaf014e6cff40997c0
baer1 <videocreator@outlook.de>
2025-04-06 05:57:11 +0200

    Limit quote length

--- quote.py
+++ quote.py
@@ -1,18 +1,23 @@
 import requests
 from typing import Literal
 from html import escape
+from time import sleep
 
-r = requests.get(
-    "https://thequoteshub.com/api/random-quote",
-    headers={
-        "Accept": "application/json",
-        "User-Agent": f"Python script (App2) using requests v{requests.__version__} (https://github.com/baerchen201/App2, videocreator@outlook.de)",
-    },
-)
-r.raise_for_status()
-json: dict[
-    Literal["text", "author", "tags", "id", "author_id"], str | list[str] | int
-] = r.json()
+while True:
+    r = requests.get(
+        "https://thequoteshub.com/api/random-quote",
+        headers={
+            "Accept": "application/json",
+            "User-Agent": f"Python script (App2) using requests v{requests.__version__} (https://github.com/baerchen201/App2, videocreator@outlook.de)",
+        },
+    )
+    r.raise_for_status()
+    json: dict[
+        Literal["text", "author", "tags", "id", "author_id"], str | list[str] | int
+    ] = r.json()
+    if len(json["text"]) < 200:
+        break
+    sleep(1)
 print(
     f"<div style=\"font-size: 2rem\">{escape(json['text']).replace("\n", "<br />")}<br /><i> - {escape(json['author'])}</i></div>"
 )
